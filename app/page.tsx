ace8ad4ab0b70b40d39e98fc339e51ce6165d8e6
Yash Sachan <yashsachan321@gmail.com>
2023-07-13 16:44:09 +0530

    added combobox

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
-import { Hero } from "../components";
+import { Hero, CustomFilter, SearchBar } from "../components";
+
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl">Car Catalogue</h1>
+          <p>Explore the Cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar></SearchBar>
+          <div className="home__filter-container">
+            <CustomFilter title="fuel"></CustomFilter>
+            <CustomFilter title="year"></CustomFilter>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,12 @@
+import { SearchManufacturer } from "@/components";
 import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+}
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
